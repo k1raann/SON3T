58331c45af24998dda8340b6b9a2d590aacd293a
k1raann <108261051+k1raann@users.noreply.github.com>
2022-08-07 23:09:25 +0530

    make it better

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
 const mongoose = require("mongoose");
 const { MONGOURI } = require("./config/valuekeys");
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'))
-    const path = require("path")
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser : true,
@@ -26,6 +18,15 @@ app.use(express.json());
 app.use(require('./routes/auth'))
 app.use(require('./routes/postrouter'))
 app.use(require('./routes/userrouter'))
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+    const path = require("path")
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(PORT, ()=>{
     console.log("Server is Running!");
